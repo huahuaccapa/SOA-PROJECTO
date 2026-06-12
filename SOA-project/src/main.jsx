ba32db24a217ecfd2114602d4c14772d243fc1c2
Kevin Huahuaccapa <huahuaccapa1119@gmail.com>
2026-06-12 00:07:35 -0500

    feat: integrar estructura principal de la aplicacion

--- SOA-project/src/main.jsx
+++ SOA-project/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './contexts/AuthContext'
+import { CartProvider } from './contexts/CartContext'
+import { NotificationProvider } from './contexts/NotificationContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NotificationProvider>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
+      </NotificationProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
