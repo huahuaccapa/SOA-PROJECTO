786b5be2199b74c0eec62bc034408b4283b563d0
kevin <huahuaccapa1119@gmail.com>
2026-06-28 01:52:45 -0500

    feat(vendor): actualizar panel de vendedor

--- SOA-project/src/pages/VendorPanel.jsx
+++ SOA-project/src/pages/VendorPanel.jsx
@@ -1,4 +1,4 @@
-// src/pages/VendorPanel.jsx
+// src\pages\VendorPanel.jsx
 import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { productService } from '../services/productService'
