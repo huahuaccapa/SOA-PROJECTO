6cf2afcb924b535b37bf3c2acac00f4d2b5dcdb2
kevin <huahuaccapa1119@gmail.com>
2026-06-28 01:52:24 -0500

    feat(cart): actualizar componente CartModal

--- SOA-project/src/components/Cart/CartModal.jsx
+++ SOA-project/src/components/Cart/CartModal.jsx
@@ -1,4 +1,4 @@
-// src/components/Cart/CartModal.jsx
+// src\components\Cart\CartModal.jsx
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useCart } from '../../contexts/CartContext'
